3a8ff743025692b981bdf7cda450354e0fbdde05
jinosh05 <jinoshprabhuraj5@gmail.com>
2023-03-26 11:49:12 +0530

    my pic image added

--- lib/constants/assets.dart
+++ lib/constants/assets.dart
@@ -3,41 +3,7 @@ class Assets {
 
   static const String _imgBase = "assets/images/";
 
-  static const String noNetworkIcon = "${_iconBase}no_internet.svg";
-
-  static const String flare = "${_iconBase}flare.svg";
-
-  static const String help = "${_iconBase}help.svg";
-
-  static const String key = "${_iconBase}key.svg";
-
-  static const String logout = "${_iconBase}logout.svg";
-
-  static const String mail = "${_iconBase}mail.svg";
-
-  static const String message = "${_iconBase}message.svg";
-
-  static const String offer = "${_iconBase}offer.svg";
-
-  static const String phone = "${_iconBase}phone.svg";
-
-  static const String privacy = "${_iconBase}privacy.svg";
-
-  static const String introBg = "${_imgBase}intro/intro_bg.svg";
-
-  static const String introBase = "${_imgBase}intro/intro_";
-
-  static const String logo = "${_imgBase}logo.png";
-
-  static const String bell = "${_iconBase}bell.svg";
-
-  static const String chat = "${_iconBase}chat.svg";
-
-  static const String globe = "${_iconBase}globe.svg";
-
-  static const String plane = "${_iconBase}plane.svg";
-
-  static const String user = "${_iconBase}user.svg";
+  static const String myPic = "${_imgBase}my_pic.jpg";
 }
 
 
--- lib/screens/dark_cv.dart
+++ lib/screens/dark_cv.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:neumorphic_cv/configs/space.dart';
+import 'package:neumorphic_cv/constants/colors.dart';
 
 class DarkCV extends StatefulWidget {
   const DarkCV({super.key});
@@ -10,6 +12,28 @@ class DarkCV extends StatefulWidget {
 class _DarkCVState extends State<DarkCV> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [AppColors.black2, AppColors.black3],
+            begin: Alignment.topLeft,
+            end: Alignment.topRight,
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: Space.all(),
+                child: Row(
+                  children: const [],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
