600bd084e9ddaafaeb2cde10ade35429722d7e11
jinosh05 <jinoshprabhuraj5@gmail.com>
2023-04-02 09:30:03 +0530

    SkillWidget refracted

--- lib/screens/dark_cv.dart
+++ lib/screens/dark_cv.dart
@@ -5,6 +5,7 @@ import 'package:neumorphic_cv/configs/ui.dart';
 import 'package:neumorphic_cv/constants/colors.dart';
 import 'package:neumorphic_cv/constants/strings.dart';
 import 'package:neumorphic_cv/env.dart';
+import 'package:neumorphic_cv/widgets/skill_widget.dart';
 
 import '../configs/app_typography.dart';
 import 'components/profile_info_header.dart';
@@ -97,91 +98,6 @@ class _DarkCVState extends State<DarkCV> {
           ),
         ),
       ),
-    );
-  }
-}
-
-class SkillWidget extends StatelessWidget {
-  const SkillWidget({
-    super.key,
-    required this.name,
-    required this.rating,
-  }) : assert(rating <= 5 && rating >= 0, "Value can't be other than 0 to 5");
-
-  final String name;
-
-  /// [rating] can be only less than or equal to 5
-  final int rating;
-
-  @override
-  Widget build(BuildContext context) {
-    return NeumorphicButton(
-      onPressed: () async {},
-      margin: Space.z,
-      padding: Space.all(0.5, 0.35),
-      style: NeumorphicStyle(
-        depth: AppDimensions.normalize(1),
-        color: AppColors.black4,
-        border: const NeumorphicBorder(color: AppColors.black5),
-      ),
-      child: Row(
-        children: [
-          Expanded(
-            child: Text(
-              name,
-              style: AppText.l1!.tsc(0.8),
-            ),
-          ),
-          for (var i = 0; i < 5; i++)
-            SkillIndicatior(
-              enabled: i < rating,
-            ),
-        ],
-      ),
-    );
-  }
-}
-
-class SkillIndicatior extends StatelessWidget {
-  const SkillIndicatior({
-    super.key,
-    required this.enabled,
-  });
-
-  final bool enabled;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: Space.hf(0.1),
-      padding: Space.all(enabled ? 0.2 : 0.4),
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        gradient: RadialGradient(
-          colors: [Colors.black, Colors.black.withOpacity(0.45)],
-        ),
-      ),
-      child: enabled
-          ? Container(
-              padding: Space.all(0.20),
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                boxShadow: [
-                  BoxShadow(
-                      spreadRadius: 2,
-                      blurRadius: 2,
-                      color: AppColors.lightBlue.withOpacity(0.5))
-                ],
-                gradient: RadialGradient(
-                  colors: [
-                    AppColors.lightBlue.withOpacity(0.85),
-                    AppColors.lightBlue,
-                    AppColors.lightBlue.withOpacity(0.85),
-                  ],
-                ),
-              ),
-            )
-          : null,
     );
   }
 }
