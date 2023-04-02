fcea5e886c480bdf36b431c68e6c99d53a7cce73
jinosh05 <jinoshprabhuraj5@gmail.com>
2023-04-02 10:37:18 +0530

    Skill Assets Icons added

--- lib/constants/assets.dart
+++ lib/constants/assets.dart
@@ -10,6 +10,14 @@ class Assets {
   static const String gmailIcon = "${_iconBase}gmail.svg";
 
   static const String linkedIn = "${_iconBase}linkedin.svg";
+
+  static const String firebase = "${_iconBase}firebase.svg";
+
+  static const String flutter = "${_iconBase}flutter.svg";
+
+  static const String python = "${_iconBase}python.svg";
+
+  static const String javascript = "${_iconBase}javascript.png";
 }
 
 
