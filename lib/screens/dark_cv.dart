14a33bc1239a1b31b87212b2ede0cff9eb72a86f
jinosh05 <jinoshprabhuraj5@gmail.com>
2023-04-02 16:43:07 +0530

    _ExperienceCard Refracted

--- lib/screens/dark_cv.dart
+++ lib/screens/dark_cv.dart
@@ -14,6 +14,8 @@ import '../configs/app_typography.dart';
 import '../widgets/skill_widget.dart';
 import 'components/profile_info_header.dart';
 
+part 'components/experience_card.dart';
+
 class DarkCV extends StatefulWidget {
   const DarkCV({super.key});
 
@@ -141,113 +143,6 @@ class _DarkCVState extends State<DarkCV> {
             ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class _ExperienceCard extends StatelessWidget {
-  const _ExperienceCard(this.data);
-  final Map data;
-
-  @override
-  Widget build(BuildContext context) {
-    return Neumorphic(
-      margin: Space.z!.b(1.5),
-      padding: Space.all(),
-      style: NeumorphicStyle(
-        shape: NeumorphicShape.concave,
-        boxShape: NeumorphicBoxShape.roundRect(
-            BorderRadius.circular(AppDimensions.normalize(7))),
-        shadowLightColor: Colors.black,
-        shadowDarkColor: AppColors.black2,
-        depth: AppDimensions.normalize(1),
-        color: AppColors.black5,
-        border: const NeumorphicBorder(color: AppColors.black5),
-        lightSource: LightSource.top,
-      ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Neumorphic(
-            margin: Space.z!.t(1),
-            padding: Space.all(0.2),
-            style: NeumorphicStyle(
-              boxShape: const NeumorphicBoxShape.circle(),
-              shape: NeumorphicShape.flat,
-              depth: AppDimensions.normalize(5),
-              intensity: 0.9,
-              shadowDarkColor: Colors.black,
-              shadowLightColor: AppColors.black5,
-              color: AppColors.black4,
-              border: const NeumorphicBorder(color: AppColors.black5),
-              lightSource: LightSource.top,
-            ),
-            child: Container(
-              clipBehavior: Clip.antiAlias,
-              decoration: const BoxDecoration(shape: BoxShape.circle),
-              child: AppImage(
-                data['logo'],
-                width: AppDimensions.normalize(15),
-              ),
-            ),
-          ),
-          Expanded(
-              child: Padding(
-            padding: Space.hf(0.25).l(0.5),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  data['company'].toUpperCase(),
-                  style: AppText.l1b,
-                ),
-                Padding(
-                  padding: Space.vf(0.25),
-                  child: Text(
-                    data['position'].toUpperCase(),
-                    style: AppText.l1b,
-                  ),
-                ),
-                Text(
-                  data['duration'],
-                  style: AppText.l1b,
-                ),
-                NeumorphicButton(
-                  onPressed: () async {
-                    String url = data['site'];
-                    if (await canLaunchUrl(Uri.parse(url))) {
-                      launchUrl(Uri.parse(url));
-                    }
-                  },
-                  margin: Space.z!.t(0.75),
-                  padding: Space.all(0.75, 0.25),
-                  style: NeumorphicStyle(
-                    depth: AppDimensions.normalize(1),
-                    color: AppColors.black2,
-                    border: const NeumorphicBorder(color: AppColors.black5),
-                  ),
-                  child: Wrap(
-                    crossAxisAlignment: WrapCrossAlignment.center,
-                    children: [
-                      Text(
-                        data['btnTitle'],
-                        style: AppText.l1!,
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          )),
-          Expanded(
-            child: Text(
-              data['experience'],
-              textAlign: TextAlign.left,
-              style: AppText.l1,
-            ),
-          ),
-        ],
       ),
     );
   }
