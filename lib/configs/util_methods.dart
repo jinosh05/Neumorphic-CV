f91444e77765e0660928dc50d157c1bbe04e62b3
jinosh05 <jinoshprabhuraj5@gmail.com>
2023-04-04 04:18:25 +0530

    Added external browser launch feature

--- lib/configs/util_methods.dart
+++ lib/configs/util_methods.dart
@@ -2,6 +2,9 @@ import 'package:url_launcher/url_launcher.dart';
 
 Future<void> launchLink(url) async {
   if (await canLaunchUrl(Uri.parse(url))) {
-    launchUrl(Uri.parse(url));
+    launchUrl(
+      Uri.parse(url),
+      mode: LaunchMode.externalApplication,
+    );
   }
 }
