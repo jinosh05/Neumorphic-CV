061fba328491f74b22ab38fa6398a696e2dbeb87
jinosh05 <jinoshprabhuraj5@gmail.com>
2023-04-04 22:45:29 +0530

    _TechnicalCard refracted

--- lib/screens/dark_cv.dart
+++ lib/screens/dark_cv.dart
@@ -18,6 +18,7 @@ import 'components/profile_info_header.dart';
 
 part 'components/experience_card.dart';
 part 'components/language_button.dart';
+part 'components/technical_card.dart';
 
 class DarkCV extends StatefulWidget {
   const DarkCV({super.key});
@@ -184,75 +185,6 @@ class _DarkCVState extends State<DarkCV> {
                 ],
               ),
               Space.yf(1.5),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class _TechnicalCard extends StatelessWidget {
-  const _TechnicalCard(this.text);
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: AnimateGradient(
-        primaryBegin: Alignment.centerLeft,
-        primaryEnd: Alignment.topRight,
-        secondaryBegin: Alignment.topRight,
-        secondaryEnd: Alignment.centerLeft,
-        duration: const Duration(
-          seconds: 2,
-        ),
-        primaryColors: const [
-          AppColors.lightBlack,
-          AppColors.black2,
-          AppColors.black6,
-          AppColors.black4,
-        ],
-        secondaryColors: const [
-          AppColors.black2,
-          AppColors.black6,
-          AppColors.black4,
-          AppColors.lightBlack,
-        ],
-        child: Container(
-          width: double.maxFinite,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(
-              AppDimensions.normalize(10),
-            ),
-          ),
-          padding: Space.all(0.75),
-          child: Row(
-            children: [
-              Container(
-                padding: Space.all(),
-                margin: Space.z!.r(0.5),
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  boxShadow: [
-                    const BoxShadow(
-                      color: Colors.red,
-                    ),
-                    BoxShadow(
-                      color: Colors.yellow,
-                      spreadRadius: -AppDimensions.normalize(1),
-                      blurRadius: AppDimensions.normalize(1),
-                    ),
-                  ],
-                ),
-              ),
-              Expanded(
-                child: Text(
-                  text,
-                  style: AppText.b3b,
-                ),
-              ),
             ],
           ),
         ),
