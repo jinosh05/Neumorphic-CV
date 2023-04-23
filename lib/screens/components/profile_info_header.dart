1897f27fb0334e51e8c8cd35374ce171075308d9
jinosh05 <jinoshprabhuraj5@gmail.com>
2023-04-23 12:43:21 +0530

    _LinksWrap adopted to mobile

--- lib/screens/components/profile_info_header.dart
+++ lib/screens/components/profile_info_header.dart
@@ -2,6 +2,7 @@ import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:neumorphic_cv/configs/app_dimensions.dart';
 import 'package:neumorphic_cv/configs/app_typography.dart';
 import 'package:neumorphic_cv/configs/space.dart';
+import 'package:neumorphic_cv/configs/space_ext.dart';
 import 'package:neumorphic_cv/constants/colors.dart';
 import 'package:neumorphic_cv/env.dart';
 import 'package:neumorphic_cv/widgets/links_buttons.dart';
@@ -18,40 +19,50 @@ class ProfileInfoHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: Space.all(1.5),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      child: Column(
         children: [
-          const ProfilePic(),
-          Space.x1!,
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Space.y!,
-                Text(
-                  Env.name,
-                  style: AppText.b2b!.gilda(),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const ProfilePic(),
+              Space.x1!,
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Space.y!,
+                    Text(
+                      Env.name,
+                      style: AppText.b2b!.gilda(),
+                    ),
+                    Space.yf(0.25),
+                    Text(
+                      Env.profession.toUpperCase(),
+                      style: AppText.b3b!.gilda().copyWith(
+                        color: AppColors.lightBlue,
+                        shadows: [const Shadow(color: AppColors.darkBlue)],
+                      ),
+                    ),
+                    Space.yf(0.25),
+                    SizedBox(
+                      width: AppDimensions.width(60),
+                      child: Text(
+                        Env.address,
+                        style: AppText.l1b,
+                      ),
+                    ),
+                    UI.isPortrait ? const SizedBox() : const _LinksWrap()
+                  ],
                 ),
-                Space.yf(0.25),
-                Text(
-                  Env.profession.toUpperCase(),
-                  style: AppText.b3b!.gilda().copyWith(
-                    color: AppColors.lightBlue,
-                    shadows: [const Shadow(color: AppColors.darkBlue)],
-                  ),
-                ),
-                Space.yf(0.25),
-                SizedBox(
-                  width: AppDimensions.width(60),
-                  child: Text(
-                    Env.address,
-                    style: AppText.l1b,
-                  ),
-                ),
-                UI.isPortrait ? const SizedBox() : const _LinksWrap()
-              ],
-            ),
-          )
+              )
+            ],
+          ),
+          !UI.isPortrait
+              ? const SizedBox()
+              : Padding(
+                  padding: Space.z!.t(1),
+                  child: const _LinksWrap(),
+                )
         ],
       ),
     );
